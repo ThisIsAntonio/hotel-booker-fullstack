6bf905deb60b8aa46ad6776608373a1c6f4022ac
Marcos Astudillo <antonio.ac86@hotmail.com>
2025-05-29 11:52:11 -0400

    feat(devops): add temporary run-migrations route

--- api/backend/urls.py
+++ api/backend/urls.py
@@ -21,7 +21,15 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.http import JsonResponse
 from django.contrib.auth import get_user_model
 from django.views.decorators.csrf import csrf_exempt
+from django.core.management import call_command
 
+@csrf_exempt
+def run_migrations(request):
+    try:
+        call_command('migrate')
+        return JsonResponse({"status": "Migraciones aplicadas con éxito"})
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=500)
 
 @csrf_exempt
 def create_admin(request):
@@ -41,4 +49,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('create-admin/', create_admin),
+    path('run-migrations/', run_migrations),
 ]
