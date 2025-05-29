5d606611c9bde56ae60d49af22c7cb6373963e7e
Marcos Astudillo <antonio.ac86@hotmail.com>
2025-05-29 13:48:07 -0400

    Temporarily re-added migration and superuser creation endpoints for PostgreSQL setup

--- api/backend/urls.py
+++ api/backend/urls.py
@@ -61,4 +61,7 @@ urlpatterns = [
     path('api/', include('reservations.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('run-migrations/', run_migrations),
+    path('collect-static/', collect_static),
+    path('create-admin/', create_admin),
 ]
