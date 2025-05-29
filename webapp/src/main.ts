cfa741359a61769fc24ca85e360a3a64fa4be8da
Marcos Astudillo <antonio.ac86@hotmail.com>
2025-05-29 16:37:47 -0400

    fix: use production environment for Angular build

--- webapp/src/main.ts
+++ webapp/src/main.ts
@@ -4,6 +4,9 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 
+import { environment } from './environments/environment';
+console.log('🧪 Current API URL:', environment.apiUrl);
+
 bootstrapApplication(AppComponent, {
   providers: [provideHttpClient(), provideRouter(routes)]
 })
